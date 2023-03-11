1497747a30b00b4ea818329b8b1519f60b9cd4d9
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2023-03-11 11:04:00 +0530

    payemnt button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:ecommerce_app/Features-Login/Views/LoginScreen.dart';
-import 'package:ecommerce_app/temp.dart';
+import 'package:ecommerce_app/PaymentButton.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
@@ -9,8 +9,8 @@ import 'package:get/route_manager.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
-  // Stripe.publishableKey =
-  //     "pk_test_51HYoWMA79opfJT5scdJaJ2mcssKI1lkkJ7EnyKIvziFx5ymYAFmOzXXnAJht2s7N3WmRQXZ1WIybAz77wtr3dks600WkvFbGwl";
+  Stripe.publishableKey =
+      "pk_test_51HYoWMA79opfJT5scdJaJ2mcssKI1lkkJ7EnyKIvziFx5ymYAFmOzXXnAJht2s7N3WmRQXZ1WIybAz77wtr3dks600WkvFbGwl";
   await GetStorage.init();
   runApp(GetMaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 }
